4928a6114cec1e2826a0ac56381ab8f2d4c3de1b
Yuri Dyachenko <pub.yurial@gmail.com>
2016-02-08 18:19:07 +0300

    add: unistd::signalfd

---
+++ signalfd.hpp
@@ -0,0 +1,20 @@
+#ifndef UNISTD_SIGNALFD_HPP
+#define UNISTD_SIGNALFD_HPP
+
+#include <sys/signalfd.h>
+
+namespace unistd
+{
+
+int signalfd(int fd, const sigset_t* mask, int flags = 0) __attribute__((warn_unused_result));;
+int signalfd(const sigset_t* mask, int flags = 0) __attribute__((warn_unused_result));;
+
+int signalfd(int fd, const sigset_t& mask, int flags = 0) __attribute__((warn_unused_result));;
+int signalfd(const sigset_t& mask, int flags = 0) __attribute__((warn_unused_result));;
+
+} //namespace unistd
+
+#include "signalfd.inc"
+
+#endif
+
